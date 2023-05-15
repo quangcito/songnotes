5fe215a458cb33ba3f0cef8fbecac5125d2abe94
Quang Duong Nguyen <qnguyen2@macalester.edu>
2023-05-15 16:42:01 -0500

    integrate basic django rest framework

--- songnotes/songnotesapi/models.py
+++ songnotes/songnotesapi/models.py
@@ -1,3 +1,24 @@
+import datetime
+
+from django.contrib import admin
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Songnote(models.Model):
+  song_name = models.CharField(max_length=200)
+  song_note = models.CharField(max_length=300)
+  pub_date = models.DateTimeField("date published")
+
+  def __str__(self) -> str:
+    return self.song_name
+
+  @admin.display(
+    boolean=True,
+    ordering="pub_date",
+    description="Published recently?",
+  )
+
+  def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- songnotes/songnotesapi/urls.py
+++ songnotes/songnotesapi/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
 from . import views
 
 app_name = "songnotesapi"
+router = routers.DefaultRouter()
+router.register(r'songnotes', views.SongnoteViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-  path('', views.index, name='index'),
+  # path('', views.index, name='index'),
+  path('', include(router.urls)),
+  path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
